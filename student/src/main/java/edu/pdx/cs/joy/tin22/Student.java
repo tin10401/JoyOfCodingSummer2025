2ef350e6142d24581ec64c71ee7c4578fdb88668
Tin Le <tin22@pdx.edu>
2025-07-09 16:35:28 -0700

    Complete Project 0 implementation

--- student/src/main/java/edu/pdx/cs/joy/tin22/Student.java
+++ student/src/main/java/edu/pdx/cs/joy/tin22/Student.java
@@ -1,53 +1,57 @@
 package edu.pdx.cs.joy.tin22;
 
 import edu.pdx.cs.joy.lang.Human;
-
 import java.util.ArrayList;
-                                                                                    
-/**                                                                                 
- * This class is represents a <code>Student</code>.                                 
- */                                                                                 
-public class Student extends Human {                                                
-                                                                                    
-  /**                                                                               
-   * Creates a new <code>Student</code>                                             
-   *                                                                                
-   * @param name                                                                    
-   *        The student's name                                                      
-   * @param classes                                                                 
-   *        The names of the classes the student is taking.  A student              
-   *        may take zero or more classes.                                          
-   * @param gpa                                                                     
-   *        The student's grade point average                                       
-   * @param gender                                                                  
-   *        The student's gender ("male", "female", or "other", case insensitive)
-   */                                                                               
-  public Student(String name, ArrayList<String> classes, double gpa, String gender) {
-    super(name);
-  }
 
-  /**                                                                               
-   * All students say "This class is too much work"
-   */
-  @Override
-  public String says() {                                                            
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
-                                                                                    
-  /**                                                                               
-   * Returns a <code>String</code> that describes this                              
-   * <code>Student</code>.                                                          
-   */                                                                               
-  public String toString() {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
+public class Student extends Human {
+    private ArrayList<String> classes;
+    private double gpa;
+    private String gender;
 
-  /**
-   * Main program that parses the command line, creates a
-   * <code>Student</code>, and prints a description of the student to
-   * standard out by invoking its <code>toString</code> method.
-   */
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-  }
+    public Student(String name, ArrayList<String> classes, double gpa, String gender) {
+        super(name);
+        this.classes = classes;
+        this.gpa = gpa;
+        this.gender = gender;
+    }
+
+    @Override
+    public String says() {
+        return "This class is too much work";
+    }
+
+    @Override
+    public String toString() {
+        return getName()
+            + " is taking "
+            + classes.size()
+            + " classes, has a GPA of "
+            + gpa
+            + ", and identifies as "
+            + gender;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.err.println("Missing command line arguments");
+            return;
+        }
+        String name = args[0];
+        ArrayList<String> classes = new ArrayList<>();
+        if (!args[1].isBlank()) {
+            for (String c : args[1].split(",")) {
+                classes.add(c);
+            }
+        }
+        double gpa;
+        try {
+            gpa = Double.parseDouble(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid GPA: " + args[2]);
+            return;
+        }
+        String gender = args[3];
+        System.out.println(new Student(name, classes, gpa, gender));
+    }
 }
+
