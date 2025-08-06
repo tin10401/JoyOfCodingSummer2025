97e31d9c3f287e100a1d3c4e2dba7175c4bf21a1
Tin Le <sololtin2001@gmail.com>
2025-08-06 16:57:07 -0700

    Create : Project4

---
+++ apptbook-web/src/it/java/edu/pdx/cs/joy/tin22/SmokeIT.java
@@ -0,0 +1,10 @@
+package edu.pdx.cs.joy.tin22;
+
+import org.junit.jupiter.api.Test;
+
+/** Placeholder so Failsafe sees at least one IT class */
+class SmokeIT {
+  @Test
+  void nothing() { /* intentionally empty */ }
+}
+
